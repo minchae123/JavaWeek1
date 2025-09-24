7333b0cd6654fa9906a14f836a5c62412742a878
Alsco_o <08minchae17@gmail.com>
2025-09-24 14:40:11 +0900

    q1

--- src/Exec/Question1.java
+++ src/Exec/Question1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Question1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        System.out.println("정수를 입력하세요: ");
         int number =  input.nextInt();
 
         if(number % 2 == 0)
