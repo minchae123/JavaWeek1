1d7fdbbf0d63c5dd8c9032f6fc166ce8310fc2fc
Alsco_o <08minchae17@gmail.com>
2025-09-24 14:47:58 +0900

    Initial commit

---
+++ src/Prac/OutputExample.java
@@ -0,0 +1,13 @@
+package Prac;
+
+public class OutputExample {
+    public static void main(String[] args) {
+        int age = 20;
+        double height = 162;
+        String name = "alsco";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " cm");
+    }
+}
