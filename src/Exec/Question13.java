a111f6f63479f961548aee07fa68deb70e04d11a
Alsco_o <08minchae17@gmail.com>
2025-09-24 15:10:25 +0900

    Q13

--- src/Exec/Question13.java
+++ src/Exec/Question13.java
@@ -6,13 +6,54 @@ public class Question13 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        System.out.print("1번 숫자 입력: ");
         int n1 =  input.nextInt();
-        int n2 = input.nextInt();
-        int n3 = input.nextInt();
+        System.out.print("+-*/ 중 입력: ");
         char oper = input.next().charAt(0);
+        System.out.print("2번 숫자 입력: ");
+        int n2 = input.nextInt();
+        System.out.print("+-*/ 중 입력: ");
+        char oper2 = input.next().charAt(0);
+        System.out.print("3번 숫자 입력: ");
+        int n3 = input.nextInt();
 
-        int result = 0;
+        double result = 0.0;
 
+        switch (oper) {
+            case '*': {
+                switch(oper2){
+                    case '*': result = n1 * n2 * n3; break;
+                    case '/': result = n1 * n2 / n3; break;
+                    case '+': result = n1 * n2 + n3; break;
+                    case '-': result = n1 * n2 - n3; break;
+                }
+            }
+            case '/' : {
+                switch(oper2){
+                    case '*': result = n1 / n3 * n2; break;
+                    case '/': result = n1 / n2 / n3; break;
+                    case '+': result = n1 / n2 - n3; break;
+                    case '-': result = n1 / n2 - n3; break;
+                }
+            }
+            case '+' : {
+                switch(oper2){
+                    case '*': result = n1 + n2 * n3; break;
+                    case '/': result = n1 / n2 / n3; break;
+                    case '+': result = n1 + n2 + n3; break;
+                    case '-': result = n1 - n2 - n3; break;
+                }
+            }
+            case '-': {
+                switch(oper2){
+                    case '*': result = n1 - n2 * n3; break;
+                    case '/': result = n1 - n2 / n3; break;
+                    case '+': result = n1 - n2 - n3; break;
+                    case '-': result = n1 - n2 - n3; break;
+                }
+            }
+        }
 
+        System.out.println(result);
     }
 }
